1fc594d6abfdc87b58f1c067a5e89117b0503a68
Andrii Riabchuk <andrii.riabchuk@devcom.com>
2021-09-01 23:48:42 +0300

    main

---
+++ settings/constants.py
@@ -0,0 +1,8 @@
+import os
+
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+BUCKET_NAME = 'test.task.bucket'
+BUCKET_FOLDER = 'data'
+FILE_TO_UPLOAD_URL = 'https://internationalnewsagency.org/wp-content/uploads/2021/07/Hanma-Baki-anime.jpg'
